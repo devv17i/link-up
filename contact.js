09fdfe671e95f1165eca0f2c4daac25b6a8aa772
devv17i <151990420+devv17i@users.noreply.github.com>
2024-01-25 11:29:52 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,9 +1,8 @@
-// app.js
-// Import specific functions from the Firebase SDK version 9
+
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-app.js';
 import { getFirestore, collection, addDoc } from 'https://www.gstatic.com/firebasejs/9.6.0/firebase-firestore.js';
 
-// Replace with your Firebase project configuration
+
 const firebaseConfig = {
     apiKey: "AIzaSyCR8FfTUGjiSXgN2XzSHjN9owwrbOX-Uss",
     authDomain: "oauth-ebafa.firebaseapp.com",
@@ -13,10 +12,10 @@ const firebaseConfig = {
     appId: "1:657639756347:web:a3f1de3c56e42a45da821e"
   };
 
-// Initialize Firebase
+
 const firebaseApp = initializeApp(firebaseConfig);
 
-// Reference to the Firestore database
+
 const db = getFirestore(firebaseApp);
 
 const send = document.getElementById("send");
@@ -26,7 +25,7 @@ send.addEventListener("click", ()=>{
     const email = document.getElementById('email').value;
     const problem = document.getElementById('problem').value;
 
-    // Add the user's information to the Firestore database
+   
     addDoc(collection(db, 'queries'), {
         name: name,
         email: email,
@@ -34,7 +33,7 @@ send.addEventListener("click", ()=>{
     })
     .then(() => {
         alert('Query submitted successfully!');
-        // You can add additional logic or redirect the user after submission
+     
     })
     .catch((error) => {
         console.error('Error adding document: ', error);
