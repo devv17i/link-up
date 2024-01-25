32e83f1baf93c7011f0285735303db639e23543c
devv17i <151990420+devv17i@users.noreply.github.com>
2024-01-25 11:36:49 +0530

    Update signOut.js

--- signOut.js
+++ signOut.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.2/firebase-app.js";
 import 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js';
 
-// Firebase configuration
+
 const firebaseConfig = {
     apiKey: "AIzaSyCR8FfTUGjiSXgN2XzSHjN9owwrbOX-Uss",
     authDomain: "oauth-ebafa.firebaseapp.com",
@@ -13,21 +13,21 @@ const firebaseConfig = {
 
   const firebaseApp = initializeApp(firebaseConfig);
 
-  // Import Firebase Auth separately
+
   import { getAuth } from 'https://www.gstatic.com/firebasejs/10.7.2/firebase-auth.js';
 
-  // Get the Auth instance
+
   const auth = getAuth(firebaseApp);
 
-  // Function to handle sign-out
+
   function signOut() {
       auth.signOut().then(() => {
-          // Sign-out successful.
+
           console.log("User signed out successfully");
-          // Redirect to the index.html page
+
           window.location.href = "./index.html";
       }).catch((error) => {
-          // An error happened.
+
           console.error("Error during sign-out:", error);
       });
   }
