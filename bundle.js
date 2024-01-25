9ee1b95ba07855c9f5e6fe0d917f04fa32a01811
devv17i <151990420+devv17i@users.noreply.github.com>
2024-01-25 11:27:21 +0530

    Update bundle.js

--- bundle.js
+++ bundle.js
@@ -1,7 +1,7 @@
 function searchCodeforces(event) {
     if (event.key === "Enter") {
         var username = document.getElementById('searchInput').value;
-        // Redirect to a new page with the Codeforces user information
+       
         window.location.href = `codeforcesUserInfo.html?username=${username}`;
     }
 }
@@ -13,10 +13,4 @@ document.getElementById("contest").addEventListener("click", viewContest);
 document.getElementById("blog").addEventListener("click", viewContest); 
 document.getElementById("stalk-list").addEventListener("click", viewContest); 
 
-// document.getElementById('blog').addEventListener('click', function() {
-//     // Replace 'your-blog-page.html' with the actual URL of your blog page
-//     window.location.href = './uploadBlog.html';
-// });
-
-// const alert = document.getElementsByClassName('alert');
 
